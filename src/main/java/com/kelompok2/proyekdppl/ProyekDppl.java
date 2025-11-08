94dc071727a4d52530bd4aa89f9db8ad8f18ff30
fithriaura <dzfdlla@gmail.com>
2025-11-08 12:12:52 +0700

    Initial commit - Login UI SIRUKAN

---
+++ src/main/java/com/kelompok2/proyekdppl/ProyekDppl.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kelompok2.proyekdppl;
+
+/**
+ *
+ * @author user
+ */
+public class ProyekDppl {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            LoginPage frame = new LoginPage();
+            frame.setLocationRelativeTo(null); 
+            frame.setVisible(true);
+        }
+    });
+    }
+}
